6c30018abf123ab471af86435297d352f0812113
Deepita Pradhan <116114906+Deepita333@users.noreply.github.com>
2026-01-27 22:31:32 +0530

    Update solution.cs

--- C# Programming/problem1/solution.cs
+++ C# Programming/problem1/solution.cs
@@ -1,75 +1,15 @@
-using System;
-
-public class Motel
+public static int totalNoOfRoomsBooked(Motel[] m, string cabFacility)
 {
-    private int motelId;
-    private string motelName;
-    private string dateOfBooking;
-    private int noOfRoomsBooked;
-    private string cabFacility;
-    private double totalBill;
+    int t = 0;
 
-    public Motel(int motelId, string motelName, string dateOfBooking,
-                 int noOfRoomsBooked, string cabFacility, double totalBill)
+    foreach (Motel i in m)
     {
-        this.motelId = motelId;
-        this.motelName = motelName;
-        this.dateOfBooking = dateOfBooking;
-        this.noOfRoomsBooked = noOfRoomsBooked;
-        this.cabFacility = cabFacility;
-        this.totalBill = totalBill;
-    }
-
-    public int GetMotelId() => motelId;
-    public string GetMotelName() => motelName;
-    public string GetDateOfBooking() => dateOfBooking;
-    public int GetNoOfRoomsBooked() => noOfRoomsBooked;
-    public string GetCabFacility() => cabFacility;
-    public double GetTotalBill() => totalBill;
-}
-
-public class HelloWorld
-{
-    public static void Main(string[] args)
-    {
-        Motel[] arr = new Motel[4];
-
-        for (int i = 0; i < 4; i++)
+        if (i.GetCabFacility().Equals(cabFacility,
+            StringComparison.OrdinalIgnoreCase)
+            && i.GetNoOfRoomsBooked() > 5)
         {
-            int a = int.Parse(Console.ReadLine());
-            string b = Console.ReadLine();
-            string c = Console.ReadLine();
-            int n = int.Parse(Console.ReadLine());
-            string d = Console.ReadLine();
-            double e = double.Parse(Console.ReadLine());
-
-            arr[i] = new Motel(a, b, c, n, d, e);
-        }
-
-        string s = Console.ReadLine();
-
-        int result = TotalNoOfRoomsBooked(arr, s);
-        if (result != 0)
-        {
-            Console.WriteLine(result);
-        }
-        else
-        {
-            Console.WriteLine("No such rooms booked");
+            t += i.GetNoOfRoomsBooked();
         }
     }
-
-    public static int TotalNoOfRoomsBooked(Motel[] arr, string s)
-    {
-        int total = 0;
-        foreach (Motel m in arr)
-        {
-            if (m.GetCabFacility().Equals(s, StringComparison.OrdinalIgnoreCase)
-                && m.GetNoOfRoomsBooked() > 5)
-            {
-                total += m.GetNoOfRoomsBooked();
-            }
-        }
-        return total;
-    }
+    return t;
 }
