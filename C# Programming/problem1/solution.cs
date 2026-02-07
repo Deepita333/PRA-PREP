56e8b54710bfebead4dd7b367aee8a63db28970b
Deepita Pradhan <116114906+Deepita333@users.noreply.github.com>
2026-02-08 01:52:09 +0530

    Update solution.cs

--- C# Programming/problem1/solution.cs
+++ C# Programming/problem1/solution.cs
@@ -1,15 +1,59 @@
-public static int totalNoOfRoomsBooked(Motel[] m, string cabFacility)
-{
-    int t = 0;
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
 
-    foreach (Motel i in m)
+using System;
+using System.Collections.Generic;
+public class Course
+{
+   public int courseId {get; set;}
+   public string courseName {get; set;}
+   public string courseAdmin {get; set;}
+   public int quiz {get; set;}
+   public int handson  {get; set;}
+  
+}
+public class HelloWorld
+{
+    public static void Main(string[] args)
     {
-        if (i.GetCabFacility().Equals(cabFacility,
-            StringComparison.OrdinalIgnoreCase)
-            && i.GetNoOfRoomsBooked() > 5)
+       
+        List<Course> l = new List<Course>();
+        for(int i =0; i<4;i++)
         {
-            t += i.GetNoOfRoomsBooked();
+            Course o = new Course();
+            m.motelId= Convert.ToInt32(Console.ReadLine());
+            m.motelName=Console.ReadLine();
+            m.dateOfBooking=Console.ReadLine();
+            m.noOfRoomsBooked=Convert.ToInt32(Console.ReadLine());
+            m.cabFacility =Console.ReadLine();
+            m.totalBill = Convert.ToDouble(Console.ReadLine());
+            l.Add(m);
+        }
+        string s = Console.ReadLine();
+        int n = totalNoOfRoomsBooked(l,s);
+        if(n!=0)
+        {
+            Console.WriteLine(n);
+        }
+        else
+        {
+            Console.WriteLine("No rooms exist");
         }
     }
-    return t;
+  public static int totalNoOfRoomsBooked(List<Motel> l, strin8g s)
+
+    {
+        int t=0;
+        foreach(var i in l)
+        {
+            if (i.cabFacility.Equals(s, StringComparison.OrdinalIgnoreCase)
+    && i.noOfRoomsBooked > 5)
+
+            {
+                   t += i.noOfRoomsBooked   ;
+
+            }
+        }
+        return t; 
+    }
 }
